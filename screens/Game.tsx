f16b7818e5a9ab6490c86b0e87e29ad0dfd207a4
deXterbed <manojmishra.deb@gmail.com>
2025-05-07 08:13:18 +0530

    Add initial project structure with TypeScript configuration, layout, and screens
    - Updated tsconfig.json to include path aliases for components and screens.
    - Modified RootLayout to include a Stack.Screen for the index.
    - Replaced placeholder content in index.tsx with StartGame component.
    - Created PrimaryButton component for reusable button functionality.
    - Added Game, GameOver, and StartGame screens with basic layouts and functionality.

---
+++ screens/Game.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function Game() {
+  return (
+    <View>
+      <Text>Game</Text>
+    </View>
+  );
+}
+
+export default Game;
---
+++ screens/StartGame.tsx
@@ -0,0 +1,57 @@
+import PrimaryButton from "@/components/buttons/Primary";
+import { useState } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+
+function StartGame() {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  const numberInputHandler = (inputText: string) => {
+    setEnteredNumber(inputText);
+  };
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={enteredNumber}
+        onChangeText={numberInputHandler}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGame;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    color: "#e4d0ff",
+    padding: 16,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
